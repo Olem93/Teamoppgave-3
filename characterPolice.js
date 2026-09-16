f0d333191bf272a49605e191983f177f5a1ad349
keithpham1313 <keithphamll@gmail.com>
2026-09-16 13:29:08 +0200

    Update characterPolice.js

--- characterPolice.js
+++ characterPolice.js
@@ -1,18 +1,44 @@
+let player = {
+    valg
+}
+
 let kontrollsjanse = 30;
 
 let bortforklaring;
 let stikkeAv;
+let resultat = "";
+
+//Sjanse for å bli vinket inn til kontroll.
 
 function policeControl(){
     if(rngMathRandom() >= kontrollsjanse){
         return "Du kjører forbi politikontrollen uten å bli vinket inn.";
     }
     else{
-        return "Du blir vinket inn til kontrollen."
+        resultat = "Du blir vinket inn til kontrollen."
+        politiOppdagelse();
     }
 }
 
+//Hva oppdager politiet?
 
+function politiOppdagelse(){
+    let oppdagelse = 100;
+    if(oppdagelse < 30){
+        resultat = "Politiet luker rev";
+    }
+    else if(oppdagelse < 60){
+        resultat = "Politiet merker at du er bereust på alkohol";
+    }
+    else{
+        resultat = "Politiet finner ingenting mistenkelig, og du får kjøre videre."
+    }
+}
+
+function politiValg(){
+
+}
+//RNG funksjon
 function rngMathRandom(){
     return Math.floor(Math.random()*100);
 }
