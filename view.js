8870d62519fc3b6fac4a08402a431c96c025d41d
Rebecka Kinn <rebeckaki@getacademy.no>
2026-09-16 14:07:21 +0200

    test

--- view.js
+++ view.js
@@ -9,7 +9,6 @@ function updateView() {
   document.getElementById("appOppgave3").innerHTML = html;
 }
 
-<<<<<<< HEAD
 function spawnItems() {
   let html = "";
   for (let i = 0; i < 16; i++) {
@@ -19,20 +18,7 @@ function spawnItems() {
     if (boxTest > 4) {
       html += /*HTML*/ `<div class="rute"></div>`;
     } else {
-      html += /*HTML*/ `<div style="cursor: pointer" class="rute" onclick="takeItem()">${items[itemTest]}</div>`;
-=======
-function spawnItems(){
-    let html = "";
-    for (let i = 0; i < 16; i++){
-        boxTest = Math.floor(Math.random() * 16)
-        itemTest = Math.floor(Math.random() * items.length)
-        console.log(itemTest)
-        if (boxTest > 4){
-            html += /*HTML*/ `<div class="rute"></div>`
-        } else {
-            html += /*HTML*/ `<div style="cursor: pointer" class="rute" onclick="takeItem()">${items[itemTest].Item}</div>`
-        }
->>>>>>> 2214bdbd8ca752343f232dd947e2c4f24e598d56
+      html += /*HTML*/ `<div style="cursor: pointer" class="rute" onclick="takeItem()">${items[itemTest].Item}</div>`;
     }
   }
 
